a0926b18c45c6889d134bf5d43f6abdf7025c703
Khôi Nguyễn <originalkhoi151@gmail.com>
2023-09-27 15:16:36 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+
+const ApiError  = require("./app/api-error");
+const app = express();
+const contactsRouter = require("./app/routes/contact.route");
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resources not found"));
+})
+
+app.use((err, req, res, next) => {
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  })
+})
+
+app.get("/",(req, res) => {
+  res.json({ message: "Welcome to contact book application." });
+});
+
+module.exports = app;
